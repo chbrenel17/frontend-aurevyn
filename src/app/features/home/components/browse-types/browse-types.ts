f196b0b9252ff153b22b6c2148bc7044875e77a7
chbrenel <charlesbrenel165@gmail.com>
2026-04-11 01:18:59 -0300

    Browse by property type section added

--- src/app/features/home/components/browse-types/browse-types.ts
+++ src/app/features/home/components/browse-types/browse-types.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-browse-types',
+  standalone: true,
   imports: [],
   templateUrl: './browse-types.html',
   styleUrl: './browse-types.css',
 })
-export class BrowseTypes {}
+
+export class BrowseTypes {
+
+  propertyTypes = [
+    { id: 1, label: 'House', url:'/images/house.png' },
+    { id: 2, label: 'Apartment', url:'/images/apartment.png' },
+    { id: 3, label: 'Condo', url:'/images/condo.png' },
+    { id: 4, label: 'Townhome', url:'/images/townhome.png' },
+  ];
+
+}
--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroSearch } from "../components/hero-search/hero-search";
 import { Featured } from '../components/featured/featured';
 import { NavBarComponent } from '../../../shared/components/nav-bar/nav-bar';
+import { BrowseTypes } from '../components/browse-types/browse-types';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroSearch, Featured, NavBarComponent],
+  imports: [HeroSearch, Featured, NavBarComponent, BrowseTypes],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })

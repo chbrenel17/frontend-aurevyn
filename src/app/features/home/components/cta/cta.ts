d5765223980403d79c92819caf2953fc8287f685
chbrenel <charlesbrenel165@gmail.com>
2026-04-16 19:27:47 -0300

    CTA and Footer sections added to Home Page

--- src/app/features/home/components/cta/cta.ts
+++ src/app/features/home/components/cta/cta.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cta',
+  standalone: true,
   imports: [],
   templateUrl: './cta.html',
   styleUrl: './cta.css',
--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -1,13 +1,15 @@
+import { Footer } from './../components/footer/footer';
 import { Component } from '@angular/core';
 import { HeroSearch } from "../components/hero-search/hero-search";
 import { Featured } from '../components/featured/featured';
 import { NavBarComponent } from '../../../shared/components/nav-bar/nav-bar';
 import { BrowseTypes } from '../components/browse-types/browse-types';
+import { Cta } from "../components/cta/cta";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroSearch, Featured, NavBarComponent, BrowseTypes],
+  imports: [HeroSearch, Featured, NavBarComponent, BrowseTypes, Cta, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })

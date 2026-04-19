9fc001e60f687cfaae4fdb5a7debfd699f8259f2
chbrenel <charlesbrenel165@gmail.com>
2026-04-19 15:05:42 -0300

    Login/Register modal page hooked to Navbar

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../shared/services/auth';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService ,
+              private router: Router
+  ) {}
+
+  form!: FormGroup;
+   ngOnInit() {
+     this.form = this.formBuilder.group({
+       email: ['', [Validators.required, Validators.email]],
+       password: ['', [Validators.required, Validators.minLength(8)]],
+     });
+   }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;   
+    }
+    this.authService.register(this.form.value).subscribe({
+      next: (response) => {
+        if (response.success) {       
+          this.router.navigate(['/login']);
+        } else {
+          this.form.setErrors({ serverError: response.message || 'Registration failed. Please try again.' });  
+        }
+      }
+    });
+  }
+}
--- src/app/shared/components/nav-bar/nav-bar.ts
+++ src/app/shared/components/nav-bar/nav-bar.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../../../features/auth/login/login';
+import { RegisterComponent } from '../../../features/auth/register/register';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent, RegisterComponent],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  showLoginModal = false;
+
+  openLoginModal() {
+    this.showLoginModal = true;
+  }
+
+  closeLoginModal() {
+    this.showLoginModal = false;
+  }
+
+}
